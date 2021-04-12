6454d281aa028dce2670ecd2e1b033e98d7efde9
saptarsi96 <saptarshi.saha0@gmail.com>
2021-04-12 19:33:10 +0530

    proc

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -143,3 +143,5 @@ EMAIL_HOST_PASSWORD = 'xyz' # host email password required
 # this process is required because google blocks apps authentication by default
 EMAIL_RECEIVING_USER = ['to@gmail.com'] # email on which you will receive messages sent from website
 
+STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
+
